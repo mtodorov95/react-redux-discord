e4d2a1ddb98ba54fca815af9f4cb5ebddacc38c1
mtodorov95 <mtodorov95@gmail.com>
2020-10-12 17:54:20 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Chat from './components/Chat';
+import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const user = useSelector((state) => state.user.user);
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      { user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </> ) : (
+          <Login />
+        )}
+      
     </div>
   );
 }
